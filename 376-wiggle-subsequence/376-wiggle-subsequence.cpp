340f3d3b85a0d8a1984bb7b031e5d955e2701cd1
Soubhik Sen <sensoubhik2001@gmail.com>
2022-07-03 11:15:41 +0530

    Time: 10 ms (30.89%), Space: 7.2 MB (43.94%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -4,35 +4,31 @@ public:
       int n = nums.size();
       if(n==1)
         return 1;
-      vector<char> diff;
-      diff.reserve(nums.size()-1);
+      vector<int> diff;
+      diff.reserve(n-1);
       for(int i=1; i<n; i++)
       {
-        char ch = '0';
+        int sign = 0;
         if(nums[i]-nums[i-1]>0)
-          ch = '+';
+          sign = 1;
         else if(nums[i]-nums[i-1]<0)
-          ch = '-';
-        diff.push_back(ch);
+          sign = -1;
+        diff.push_back(sign);
       }
-      int counter = 0;
-      char prev = '0';
+      
+      int counter = 0, prev = 0;
       for(int i=0; i< diff.size(); i++)
       {
-        if(prev == '0' && diff[i] != '0')
+        int curr = diff[i];
+        if(prev == 0 && curr!= 0)
         {
           counter++;
-          prev = diff[i];
+          prev = curr;
         }
-        else if(prev == '+' && diff[i] == '-')
+        else if(prev*curr<0)
         {
           counter++;
-          prev = diff[i];
-        }
-        else if(prev == '-' && diff[i] == '+')
-        {
-          counter++;
-          prev = diff[i];
+          prev = curr;
         }
       }
       return counter+1;
