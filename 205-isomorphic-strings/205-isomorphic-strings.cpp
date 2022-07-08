f410af7a8ca63b97c5889a649cf0f62fe4169db0
Soubhik Sen <sensoubhik2001@gmail.com>
2022-07-08 13:16:33 +0530

    Time: 3 ms (97.15%), Space: 6.8 MB (98.81%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-      //array to store mapping of ascii chars
       int mapping[256];
-      //initialising it to -1
       for(int i=0; i<256; i++)
         mapping[i] = -1;
       
       for(int i=0; i<s.length(); i++)
       {
-        //if a new char is encountered, map it to the corresponding char in string t
-        if(mapping[int(s[i])] == -1)
-          mapping[int(s[i])] = int(t[i]);
-        
-        //otherwise, check whether it follows the previous mapping rule
-        //if it does not, return false
-        else if(mapping[int(s[i])]!=int(t[i]))
+        if(mapping[s[i]-'\0']==-1)
+          mapping[s[i]-'\0']=t[i]-'\0';
+        else if(mapping[s[i]-'\0']!=t[i]-'\0')
           return false;
       }
-      //there could also be same mappings for more than one char
-      //to check it, we would sort the mapping array and find any duplicate values
-      sort(mapping,mapping+256);
+      sort(mapping, mapping+256);
       for(int i=0; i<255; i++)
       {
-        //avoiding -1 values
-        if(mapping[i] == -1)
+        if(mapping[i]==-1)
           continue;
-        //if duplicate mapping found, return false
-        if(mapping[i] == mapping[i+1])
+        else if(mapping[i]==mapping[i+1])
           return false;
       }
       return true;
