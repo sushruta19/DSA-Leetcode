7c3cf01a67a740fd35af03f93b49c7efd5ded57b
Soubhik Sen <sensoubhik2001@gmail.com>
2022-07-08 12:03:11 +0530

    Time: 68 ms (8.93%), Space: 14.7 MB (39.29%) - LeetHub

---
+++ 929-unique-email-addresses/929-unique-email-addresses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+      set<string> mailset;
+      for(auto email : emails)
+      {
+        string newEmail = "";
+        for(auto ch : email)
+        {
+          if(ch == '.')continue;
+          if(ch == '@' || ch == '+')break;
+          newEmail += ch;
+        }
+        newEmail += email.substr(email.find('@'));
+        mailset.insert(newEmail);
+      }
+      return mailset.size();
+    }
+};
