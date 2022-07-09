1620b4c10f511c9a69dd87702ef72a565acebbd1
Soubhik Sen <sensoubhik2001@gmail.com>
2022-07-09 12:18:55 +0530

    Added solution - LeetHub

---
+++ Polynomial Addition - GFG/polynomial-addition.cpp
@@ -0,0 +1,180 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int coeff;
+    int pow;
+    struct Node* next;
+    
+    Node(int c, int p){
+        coeff = c;
+        pow = p;
+        next = NULL;
+    }
+    
+};
+
+
+void append(struct Node** head_ref, struct Node **tail_ref,
+            int new_data, int new_data1)
+{
+    struct Node* new_node = new Node(new_data, new_data1);
+    
+    if (*head_ref == NULL)
+        *head_ref = new_node;
+    else
+        (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+
+void printList(struct Node *head)
+{
+    struct Node *temp = head;
+    
+    while (temp != NULL)
+    {
+       printf("%d %d ", temp->coeff, temp->pow);
+       temp  = temp->next;
+    }
+}
+
+Node* addPolynomial(Node *p1, Node *p2);
+
+
+void create_node(int x, int y, struct Node **temp)
+{
+    struct Node *r, *z;
+    z = *temp;
+    if(z == NULL)
+    {
+        r = new Node(x, y);
+        *temp = r;
+        r->next = NULL;
+    }
+    else
+    {
+        r->next = new Node(x, y);
+        r = r->next;
+        r->next = NULL;
+    }
+}
+ 
+
+/* Driver program to test above function*/
+
+ // } Driver Code Ends
+/* Structure of Node used
+struct Node
+{
+    int coeff;
+    int pow;
+    struct Node* next;
+    
+    Node(int c, int p){
+        coeff = c;
+        pow = p;
+        next = NULL;
+    }
+    
+};
+*/
+
+class Solution{
+  private:
+    void append(Node** head_ref, int c, int p)
+    {
+        if(*head_ref == NULL)
+        {
+            *head_ref = new Node(c,p);
+            return;
+        }
+        Node* current = *head_ref;
+        while(current->next!=NULL)
+            current = current->next;
+        current->next = new Node(c,p);
+    }
+  public:
+    /* The below method print the required sum of polynomial
+    p1 and p2 as specified in output  */
+    Node* addPolynomial(Node *p1, Node *p2)
+    {
+        Node* l = p1, *r = p2, *add=NULL;
+        while(l!=NULL && r!=NULL)
+        {
+            if(l->pow > r->pow)
+            {
+                append(&add,l->coeff, l->pow);
+                l = l->next;
+            }
+            else if(l->pow < r->pow)
+            {
+                append(&add, r->coeff, r->pow);
+                r = r->next;
+            }
+            else
+            {
+                append(&add, l->coeff + r->coeff, l->pow);
+                l = l->next;
+                r = r->next;
+            }
+        }
+        while(l!=NULL)
+        {
+            append(&add, l->coeff, l->pow);
+            l = l->next;
+        }
+        while(r!=NULL)
+        {
+            append(&add, r->coeff, r->pow);
+            r = r->next;
+        }
+        return add;
+        
+    }
+};
+
+
+// { Driver Code Starts.
+int main()
+{
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        struct Node *poly1 = NULL, *poly2 = NULL, *poly = NULL;
+        struct Node *tail1  = NULL,*tail2 = NULL;
+        int n;
+        cin>>n;
+        for(int i=0;i<n;i++)
+        {
+            int x,y;
+            cin>>x>>y;
+            	append(&poly1,&tail1,x,y);
+        }
+        
+         int m;
+        cin>>m;
+        for(int i=0;i<m;i++)
+        {
+            int x,y;
+            cin>>x>>y;
+            	append(&poly2,&tail2,x,y);
+        }
+        Solution obj;
+        Node *sum = obj.addPolynomial(poly1,poly2);
+        for(Node* ptr=sum ; ptr ; ptr=ptr->next  )
+        {
+            // printing polynomial
+            cout<< ptr->coeff << "x^" << ptr->pow;
+            if(ptr->next) cout<<" + ";
+        }
+        cout<<endl;
+       
+    }
+
+}
+  // } Driver Code Ends
